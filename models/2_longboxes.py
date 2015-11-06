1b0f90732aa19e8de246c4c18b200ef5d6e519f7
a-random-oracle <ra728@york.ac.uk>
2015-11-06 13:24:42 +0000

    Added tables for users, boxes and comics.
    This includes test data.

---
+++ models/2_longboxes.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# Define database tables
+
+## Users
+db.define_table('users',
+    Field('username', type='string'),
+    Field('display_name', type='string'),
+    Field('password', type='password')
+)
+
+## Boxes
+db.define_table('boxes',
+    Field('user_id', type='reference users'),
+    Field('name', type='string'),
+    Field('creation_date', type='date', default=request.now),
+    Field('visible', type='boolean')
+)
+
+## Comics
+db.define_table('comics',
+    Field('box_id', type='reference boxes'),
+    Field('image', type='upload'),
+    Field('title', type='string'),
+    Field('issue_no', type='integer'),
+    Field('writers', type='list:string'),
+    Field('artists', type='list:string'),
+    Field('publisher', type='string'),
+    Field('description', type='string')
+)
