f590e2dca89d53b1d9203f2abba1601e1d835894
a-random-oracle <ra728@york.ac.uk>
2015-11-17 02:57:33 +0000

    Updated boxes to point to db.auth_user users.

--- models/2_tables.py
+++ models/2_tables.py
@@ -2,21 +2,14 @@
 
 # Define database tables
 
-## Users
-db.define_table('users',
-    Field('username', type='string'),
-    Field('display_name', type='string'),
-    Field('password', type='password')
-)
-
 ## Boxes
 db.define_table('boxes',
-    Field('user_id', type='reference users'),
+    Field('user_id', type='reference auth_user'),
     Field('name', type='string'),
     Field('creation_date', type='date', default=request.now),
     Field('visible', type='boolean')
 )
-
+ 
 ## Comics
 db.define_table('comics',
     Field('box_id', type='reference boxes'),
